df6c0a0bff28ffbbee89325c806c3e1b19ab50e8
Ivan Dolgopolov <fertexop@yandex.ru>
2026-05-24 13:27:49 +0700

    feat(products): добавил CRUD каталога товаров

---
+++ manager-crud/manager-app/src/main/java/ivan/dev/manager/service/ProductService.java
@@ -0,0 +1,18 @@
+package ivan.dev.manager.service;
+
+import ivan.dev.manager.entity.Product;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    List<Product> findAllProducts();
+
+    Product createProduct(String title, String details);
+
+    Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+}
